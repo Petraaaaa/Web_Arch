6006c743a0d9d1b0fce35ce843ae4ba946f0f2a8
Petraaaaa <petsarps@gmail.com>
2021-07-07 10:06:34 +0000

    first commit

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+    // define('DB_HOST','localhost');
+    // define('DB_USER','root');
+    // define('DB_PASS',''); 
+    // define('DB_NAME','web_arch_db');
+
+
+
+
+
+    define('DB_HOST','localhost');
+    define('DB_USER','id17201853_petra');
+    define('DB_PASS','KpDDWfr<F7Zr/S+K'); 
+    define('DB_NAME','id17201853_arch123');
+
+    ?>
